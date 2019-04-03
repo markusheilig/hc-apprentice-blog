b01bf91525434844b0e95bc8dddcec441d06d497
Markus Heilig <mheilig@hcmb0152010322.hc.lan>
2019-04-03 18:03:26 +0200

    update welcome text with my name & job

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,8 +51,7 @@ const IndexPage = ({
       <Hero>
         <h1>Hi.</h1>
         <p>
-          I&apos;m John Doe, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
+          I&apos;m Markus, a software engineer apprentice at HolidayCheck.
         </p>
         <Link to="/contact">
           <Button big>
