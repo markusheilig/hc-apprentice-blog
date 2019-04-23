7ce080777b44794dbc4dcbb08bd96b148947e13b
Markus Heilig <markusheilig@gmx.net>
2019-04-23 16:02:54 +0200

    update blog configuration (title, banner, twittername etc)

--- config/index.js
+++ config/index.js
@@ -1,21 +1,21 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteTitleManifest: 'MinimalBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'My Apprenticeship @HolidayCheck', // Navigation and Site Title
+  siteTitleAlt: 'HolidayCheck Apprenticeship', // Alternative Site title for SEO
+  siteTitleManifest: 'ApprenticeshipBlog',
+  siteUrl: 'https://markusheilig.netlify.de', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'Blog about my apprenticeship at HolidayCheck.', // Your site description
+  author: 'Markus Heilig', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
+  userTwitter: '@markusheilig2', // Twitter Username - Optional
+  // ogSiteName: 'minimal', // Facebook Site Name - Optional
   ogLanguage: 'en_US', // Facebook Language
   googleAnalyticsID: 'UA-47519312-6',
 
